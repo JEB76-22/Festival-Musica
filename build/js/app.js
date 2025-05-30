494fb0f69188381dfa92282114c630173f460dad
Javi Coder <j.e.b.inter@gmail.com>
2025-05-30 19:52:54 -0300

    creando y modelando Galeria

--- build/js/app.js
+++ build/js/app.js
@@ -1,19 +1,97 @@
 
 document.addEventListener("DOMContentLoaded", function(){
 
-    crearGaleria()
+    crearGaleria();
+    navegacionFija();
+    resaltarEnlace();
 });
+
 
 function crearGaleria(){
     const cantidad_imagenes = 16;
     const galeria = document.querySelector(".galeria-imagenes");
     
-    for (let i = 0; i <= cantidad_imagenes; i++) {
-        
+    for (let i = 1; i <= cantidad_imagenes; i++) {
         const imagen = document.createElement("IMG");
         imagen.src = `src/img/gallery/full/${i}.jpg`;
-        imagen.alt =  "imagen-galeria"
+        imagen.alt =  "imagen-galeria";
+
+        //Event Handler:responder a un click
+        imagen.onclick = function(){
+            mostrarImagen(i);
+        }
+
+
         galeria.appendChild(imagen);
-        // console.log(imagen)
     }
+}
+function mostrarImagen(i){
+    const imagen = document.createElement("IMG");
+    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.alt =  "imagen-galeria";
+
+    //Generar modal
+    const modal = document.createElement("DIV");
+    modal.classList.add("modal");
+    modal.onclick = cerrarModal;
+
+    // Boton cerrar modal
+    const cerrarModalBtn = document.createElement("BUTTON");
+    cerrarModalBtn.textContent = "X";
+    cerrarModalBtn.classList.add("btn-cerrar");
+    cerrarModalBtn.onclick = cerrarModal
+
+    modal.appendChild(imagen);
+    modal.appendChild(cerrarModalBtn);
+    
+    //Agregar al HTml
+    const body = document.querySelector("body");
+    body.classList.add("overflow-hidden");
+    body.appendChild(modal);
+}
+
+function cerrarModal(){
+    const modal = document.querySelector(".modal");
+    modal.classList.add("fade-out");
+    setTimeout(() => {
+        modal?.remove();
+
+        const body = document.querySelector("body");
+        body.classList.remove("overflow-hidden");
+    }, 500);
+}
+function  navegacionFija(){
+    const header = document.querySelector(".header");
+    const sobreFestival = document.querySelector(".sobre-festival");
+    
+    window.addEventListener("scroll", function(){
+        if(sobreFestival.getBoundingClientRect().bottom < 1){
+            header.classList.add("fixed");
+        }else{
+            header.classList.remove("fixed");
+
+        }
+    })
+}
+
+function resaltarEnlace(){
+    document.addEventListener("scroll", ()=>{
+        const sections = document.querySelectorAll("section");
+        const navLinks = document.querySelectorAll(".navegacion-principal a");
+
+        let actual ="";
+        sections.forEach(section =>{
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if(window.scrollY >= (sectionTop - sectionHeight / 3)){
+            actual = section.id
+        }
+        });
+        navLinks.forEach(link =>{
+            link.classList.remove("active");
+            if(link.getAttribute("href") === "#" + actual){
+                link.classList.add("active");
+            }
+        })
+    })
 }
